2a09af46b8766e9beb8aaf05eecbee255b5f061a
stevo <spolin44@gmail.com>
2024-08-28 18:18:04 -0400

    added rate limit retries

--- webhooker/webhook.go
+++ webhooker/webhook.go
@@ -3,11 +3,15 @@ package webhooker
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/disgoorg/disgo/discord"
+	"github.com/tidwall/gjson"
 )
 
 type ContentType string
@@ -17,13 +21,44 @@ const (
 	ContentTypeMultipart ContentType = "multipart/form-data"
 )
 
-func Send[T discord.WebhookMessageCreate | []byte | string](webhookURL string, payload T, contentType ...ContentType) (err error) {
-	var bodyBytes []byte
-	var cType ContentType = ContentTypeJSON
+type WebhookOpts struct {
+	rateLimitRetries int
+	rateLimitDelay   time.Duration
+	contentType      ContentType
+}
 
-	if len(contentType) == 1 {
-		cType = contentType[0]
+type WebhookOptFunc func(opts *WebhookOpts)
+
+func DefaultWebhookOpts() *WebhookOpts {
+	return &WebhookOpts{
+		rateLimitRetries: 0,
+		rateLimitDelay:   0,
+		contentType:      ContentTypeJSON,
 	}
+}
+
+func RateLimitWebhookOpt(retries int, delay time.Duration) WebhookOptFunc {
+	return func(opts *WebhookOpts) {
+		opts.rateLimitRetries = retries
+		opts.rateLimitDelay = delay
+	}
+}
+
+func ContentTypeWebhookOpt(contentType ContentType) WebhookOptFunc {
+	return func(opts *WebhookOpts) {
+		opts.contentType = contentType
+	}
+}
+
+func Send[T discord.WebhookMessageCreate | []byte | string](webhookURL string, payload T, opts ...WebhookOptFunc) (err error) {
+	o := DefaultWebhookOpts()
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	var bodyBytes []byte
+	contentType := o.contentType
+
 	switch payload := any(payload).(type) {
 	case discord.WebhookMessageCreate:
 		body, err := payload.ToBody()
@@ -38,7 +73,7 @@ func Send[T discord.WebhookMessageCreate | []byte | string](webhookURL string, p
 			}
 		case *discord.MultipartBuffer:
 			bodyBytes = body.Buffer.Bytes()
-			cType = ContentTypeMultipart
+			contentType = ContentTypeMultipart
 		}
 	case string:
 		bodyBytes = []byte(payload)
@@ -46,32 +81,50 @@ func Send[T discord.WebhookMessageCreate | []byte | string](webhookURL string, p
 		return fmt.Errorf("unsupported payload type")
 	}
 
-	client := http.Client{}
-	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(bodyBytes))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", string(cType))
+	retries := 0
+	for {
+		client := http.Client{}
+		req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(bodyBytes))
+		if err != nil {
+			return err
+		}
+		req.Header.Set("Content-Type", string(contentType))
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
+		if resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusOK {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return err
+			}
+			if resp.StatusCode == 429 && o.rateLimitRetries > 0 && retries < o.rateLimitRetries {
+				resJ := gjson.ParseBytes(body)
+				if strings.Contains(resJ.Get("message").Str, "rate limited") {
+					secs := resJ.Get("retry_after").Float()
+					time.Sleep(time.Second*time.Duration(secs) + time.Millisecond*100 + o.rateLimitDelay)
+					retries++
+					continue
+				} else {
+					return errors.New("invalid rate limit response, status code")
+				}
+			}
+			return fmt.Errorf("failed to send webhook, status code: %d, body: %s", resp.StatusCode, string(body))
+		}
+		return nil
 	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to send webhook, status code: %d", resp.StatusCode)
-	}
-	return nil
 }
 
-func SendWithWait[T discord.WebhookMessageCreate | []byte | string](webhookURL string, payload T, contentType ...ContentType) (m discord.Message, err error) {
-	var bodyBytes []byte
-	var cType ContentType = ContentTypeJSON
-
-	if len(contentType) == 1 {
-		cType = contentType[0]
+func SendWithWait[T discord.WebhookMessageCreate | []byte | string](webhookURL string, payload T, opts ...WebhookOptFunc) (m discord.Message, err error) {
+	o := DefaultWebhookOpts()
+	for _, opt := range opts {
+		opt(o)
 	}
+	var bodyBytes []byte
+	contentType := o.contentType
+
 	switch payload := any(payload).(type) {
 	case discord.WebhookMessageCreate:
 		body, err := payload.ToBody()
@@ -86,7 +139,7 @@ func SendWithWait[T discord.WebhookMessageCreate | []byte | string](webhookURL s
 			}
 		case *discord.MultipartBuffer:
 			bodyBytes = body.Buffer.Bytes()
-			cType = ContentTypeMultipart
+			contentType = ContentTypeMultipart
 		}
 	case string:
 		bodyBytes = []byte(payload)
@@ -94,37 +147,54 @@ func SendWithWait[T discord.WebhookMessageCreate | []byte | string](webhookURL s
 		return m, fmt.Errorf("unsupported payload type")
 	}
 
-	client := http.Client{}
-	req, err := http.NewRequest("POST", webhookURL+"?wait=true", bytes.NewBuffer(bodyBytes))
-	if err != nil {
-		return m, err
-	}
-	req.Header.Set("Content-Type", string(cType))
+	retries := 0
+	for {
+		client := http.Client{}
+		req, err := http.NewRequest("POST", webhookURL+"?wait=true", bytes.NewBuffer(bodyBytes))
+		if err != nil {
+			return m, err
+		}
+		req.Header.Set("Content-Type", string(contentType))
+		resp, err := client.Do(req)
+		if err != nil {
+			return m, err
+		}
+		defer resp.Body.Close()
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return m, err
-	}
-	defer resp.Body.Close()
+		if resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusOK {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return m, err
+			}
+			if resp.StatusCode == 429 && o.rateLimitRetries > 0 && retries < o.rateLimitRetries {
+				resJ := gjson.ParseBytes(body)
+				if strings.Contains(resJ.Get("message").Str, "rate limited") {
+					secs := resJ.Get("retry_after").Float()
+					time.Sleep(time.Second*time.Duration(secs) + time.Millisecond*100 + o.rateLimitDelay)
+					retries++
+					continue
+				} else {
+					return m, errors.New("invalid rate limit response, status code")
+				}
+			}
+			return m, fmt.Errorf("failed to send webhook, status code: %d, body: %s", resp.StatusCode, string(body))
+		}
 
-	if resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusOK {
-		return m, fmt.Errorf("failed to send webhook, status code: %d", resp.StatusCode)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return m, err
+		}
+		return m, json.Unmarshal(body, &m)
 	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return m, err
-	}
-	return m, json.Unmarshal(body, &m)
 }
 
-func Edit[T discord.WebhookMessageUpdate | []byte | string](webhookURL string, msgID string, payload T, contentType ...ContentType) (err error) {
-	var bodyBytes []byte
-	var cType ContentType = ContentTypeJSON
-
-	if len(contentType) == 1 {
-		cType = contentType[0]
+func Edit[T discord.WebhookMessageUpdate | []byte | string](webhookURL string, msgID string, payload T, opts ...WebhookOptFunc) (err error) {
+	o := DefaultWebhookOpts()
+	for _, opt := range opts {
+		opt(o)
 	}
+	var bodyBytes []byte
+	contentType := o.contentType
 
 	switch payload := any(payload).(type) {
 	case discord.WebhookMessageUpdate:
@@ -140,7 +210,7 @@ func Edit[T discord.WebhookMessageUpdate | []byte | string](webhookURL string, m
 			}
 		case *discord.MultipartBuffer:
 			bodyBytes = body.Buffer.Bytes()
-			cType = ContentTypeMultipart
+			contentType = ContentTypeMultipart
 		}
 	case string:
 		bodyBytes = []byte(payload)
@@ -148,22 +218,38 @@ func Edit[T discord.WebhookMessageUpdate | []byte | string](webhookURL string, m
 		return fmt.Errorf("unsupported payload type")
 	}
 
-	client := http.Client{}
-	req, err := http.NewRequest("PATCH", fmt.Sprintf("%s/messages/%s", webhookURL, msgID), bytes.NewBuffer(bodyBytes))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", string(cType))
+	retries := 0
+	for {
+		client := http.Client{}
+		req, err := http.NewRequest("PATCH", fmt.Sprintf("%s/messages/%s", webhookURL, msgID), bytes.NewBuffer(bodyBytes))
+		if err != nil {
+			return err
+		}
+		req.Header.Set("Content-Type", string(contentType))
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
+		if resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusOK {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return err
+			}
+			if resp.StatusCode == 429 && o.rateLimitRetries > 0 && retries < o.rateLimitRetries {
+				resJ := gjson.ParseBytes(body)
+				if strings.Contains(resJ.Get("message").Str, "rate limited") {
+					secs := resJ.Get("retry_after").Float()
+					time.Sleep(time.Second*time.Duration(secs) + time.Millisecond*100 + o.rateLimitDelay)
+					retries++
+					continue
+				} else {
+					return errors.New("invalid rate limit response, status code")
+				}
+			}
+			return fmt.Errorf("failed to send webhook, status code: %d, body: %s", resp.StatusCode, string(body))
+		}
+		return nil
 	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to update webhook, status code: %d", resp.StatusCode)
-	}
-
-	return
 }
--- webhooker/webhook_test.go
+++ webhooker/webhook_test.go
@@ -1,8 +1,10 @@
 package webhooker_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/disgoorg/disgo/discord"
 	"github.com/joho/godotenv"
@@ -27,4 +29,26 @@ func TestSendAndEdit(t *testing.T) {
 	is.NoErr(webhooker.Edit(webhookURL, m.ID.String(), discord.WebhookMessageUpdate{
 		Content: structures.Ptr("test2"),
 	}))
+}
+
+func TestWebhookRateLimit(t *testing.T) {
+	is := is.New(t)
+	is.NoErr(godotenv.Load())
+	webhookURL := os.Getenv("WEBHOOK_URL")
+
+	for i := 0; i < 10; i++ {
+		is.NoErr(webhooker.Send(webhookURL, discord.WebhookMessageCreate{
+			Content: fmt.Sprintf("test %d", i),
+		}, webhooker.RateLimitWebhookOpt(1, time.Second*1)))
+	}
+	for i := 0; i < 10; i++ {
+		m, err := webhooker.SendWithWait(webhookURL, discord.WebhookMessageCreate{
+			Content: fmt.Sprintf("test %d", i),
+		}, webhooker.RateLimitWebhookOpt(1, time.Second*1))
+		is.NoErr(err)
+		is.Equal(m.Content, fmt.Sprintf("test %d", i))
+		is.NoErr(webhooker.Edit(webhookURL, m.ID.String(), discord.WebhookMessageUpdate{
+			Content: structures.Ptr(fmt.Sprintf("test %d", i+1)),
+		}, webhooker.RateLimitWebhookOpt(1, time.Second*1)))
+	}
 }
